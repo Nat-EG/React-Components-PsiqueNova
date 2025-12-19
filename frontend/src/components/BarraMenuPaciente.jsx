74a9d2f6a654283be560c5caf97a49b70ca4878f
Paola-1599 <anyi_pgomeza@soy.sena.edu.co>
2025-12-19 16:24:38 -0500

    Se agrega el componente barra menu paciente y unas correciones en la barra menu admin y psicologo

---
+++ frontend/src/components/BarraMenuPaciente.jsx
@@ -0,0 +1,37 @@
+import stylesBarraMenuPaciente from "../styles/BarraMenuPaciente.module.css";
+
+function BarraMenuPaciente() {
+
+// Estructura del componente BarraMenuPaciente
+  return (
+    <div className={stylesBarraMenuPaciente["div-padre-barraMenu"]}>
+      <div className={stylesBarraMenuPaciente["div-barra"]}>
+        <h1>¡Te damos la bienvenidad!</h1>
+        <a href="/inicio">Inicio</a>
+        <hr className={stylesBarraMenuPaciente.hrPersonalizada} />
+
+        <div className={stylesBarraMenuPaciente["opcion-menu"]}>
+          <img src="IconoCatalogoServicios.svg" alt="" /> <a href="/catalogo-servicios">Catálogo de Servicios</a>
+        </div>
+        <hr className={stylesBarraMenuPaciente.hrPersonalizada} />
+
+        <div className={stylesBarraMenuPaciente["opcion-menu"]}>
+          <img src="IconoMiLugarSeguro.svg" alt="" /> <a href="/mi-lugar-seguro">Mi Lugar Seguro</a>
+        </div>
+        <hr className={stylesBarraMenuPaciente.hrPersonalizada} />
+      </div>
+
+      <div className={stylesBarraMenuPaciente["div-barra"]}>
+        <div className={stylesBarraMenuPaciente["opcion-menu"]}>
+          <img src="IconoConfig.svg" alt="" /> <a href="/configuracion">Ajustes</a>
+        </div>
+        <div className={stylesBarraMenuPaciente["opcion-menu"]}>
+          <img src="IconoCerrarSesion.svg" alt="" /> <a href="/cerrar-sesion">Cerrar Sesión</a>
+        </div>
+      </div>
+        
+    </div>
+  );
+}
+
+export default BarraMenuPaciente;
